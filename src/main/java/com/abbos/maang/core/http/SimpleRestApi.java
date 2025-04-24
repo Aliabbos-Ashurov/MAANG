8555b6584610d68d57411fc4d264126c6176c1ae
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-04-24 10:27:18 +0500

    server java core

---
+++ src/main/java/com/abbos/maang/core/http/SimpleRestApi.java
@@ -0,0 +1,54 @@
+package com.abbos.maang.core.http;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+/**
+ * @author Aliabbos Ashurov
+ * @version 1.0
+ * @since 2025-04-03
+ */
+public class SimpleRestApi {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/hello", new HelloHandler());
+        server.createContext("/json", new JsonHandler());
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started at http://localhost:8080/");
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello, World!";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
+    }
+
+    static class JsonHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String jsonResponse = "{ \"message\": \"Hello, JSON!\" }";
+                exchange.getResponseHeaders().set("Content-Type", "application/json");
+                exchange.sendResponseHeaders(200, jsonResponse.length());
+                try (OutputStream os = exchange.getResponseBody()) {
+                    os.write(jsonResponse.getBytes());
+                }
+            } else {
+                exchange.sendResponseHeaders(405, -1); // Method Not Allowed
+            }
+        }
+    }
+}
